cfeeac97c6870bb8b1dbe1f733abea7414e4d660
Nutti Saelor <nutti.sae@truedigital.com>
2024-08-03 22:47:23 +0700

    feat: add create-product and placeorder api

--- producer.js
+++ producer.js
@@ -1,16 +1,9 @@
 const express = require("express");
-const { Kafka } = require("kafkajs");
 const { Order, Product, sequelize } = require("./schema");
+const { where } = require("sequelize");
 
 const app = express();
 const port = 8000;
-
-const kafka = new Kafka({
-  clientId: "express-app",
-  brokers: ["localhost:9092", "localhost:9092"],
-});
-
-const producer = kafka.producer();
 
 app.use(express.json());
 
@@ -28,58 +21,29 @@ app.post("/api/create-product", async (req, res) => {
 });
 
 app.post("/api/placeorder", async (req, res) => {
+  const { userId, productId } = req.body;
   try {
-    const { productId, userId } = req.body;
-
-    const product = await Product.findOne({
-      where: {
-        id: productId,
-      },
-    });
+    const product = await Product.findOne({ where: { id: productId } });
 
     if (product.amount <= 0) {
-      res.json({
-        message: "product out of stock",
-      });
-      return false;
+      res.status(400).json({ message: "Product out of stock" });
     }
 
-    // reduce amount
     product.amount -= 1;
     await product.save();
 
-    // create order with status pending
     const order = await Order.create({
-      productId: product.id,
       userLineUid: userId,
       status: "pending",
+      productId,
     });
-
-    const orderData = {
-      productName: product.name,
-      userId,
-      orderId: order.id,
-    };
-
-    await producer.connect();
-    await producer.send({
-      topic: "message-topic",
-      messages: [
-        {
-          value: JSON.stringify(orderData),
-        },
-      ],
-    });
-    await producer.disconnect();
 
     res.json({
-      message: `buy product ${product.name} successful. waiting message for confirm.`,
+      order,
+      message: "Create order successfully",
     });
   } catch (error) {
-    res.json({
-      message: "something wront",
-      error,
-    });
+    res.status(500).json({ error });
   }
 });
 
